6aa48bc61bb664356cd6f90cb1252935dc4126f4
souranourah525 <souranourah525@gmail.com>
2026-05-16 01:04:53 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'empreinte-v1';
+const CACHE_NAME = 'empreinte-v2';
 const ASSETS = [
   'index.html',
   'style.css',
@@ -7,8 +7,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
